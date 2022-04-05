7034dcb6e9de9bb600ab1d32e450a8bf2321b52d
SRigby25 <sian.rigby@hotmail.com.au>
2022-04-05 19:51:14 +0800

    5/4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  let [message, setMessage] = useState(
+    "java script yada yada"
   );
+
+  function handleClick() {
+    setMessage("somethig else");
+  }
+  
+  // let message = "hello world"
+  return (
+  <div>
+    <h1>{message}</h1>;)
+    <button onCLick ={handleClick}>Update the message </button>
+  </div>
+);
 }
 
 export default App;
